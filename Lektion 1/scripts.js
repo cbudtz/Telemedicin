c330f6b1903dd774dc8afdb1d9b978fe12ffe0d3
Christian <chrbudtz@gmail.com>
2024-03-05 14:16:52 +0100

    first commit

---
+++ Lektion 1/scripts.js
@@ -0,0 +1,19 @@
+const search = () => {
+  fetch(
+    "https://api.dataforsyningen.dk/adgangsadresser?vejnavn=Rødkildevej"
+  ).then((response) =>
+    response.json().then((data) => {
+      console.log(data);
+      let html = "<ul>";
+      data.forEach((element) => {
+        html += "<li>" + element.adressebetegnelse + "</li>";
+      });
+      html += "</ul>";
+      document.getElementById("found").innerHTML = html;
+    })
+  );
+};
+
+window.onload = () => {
+  document.getElementById("searchButton").addEventListener("click", search);
+};
