5b4bb69042e1e01b3a8789102a323772cf2a206f
Christian <chrbudtz@gmail.com>
2024-03-11 20:27:35 +0100

    end solution

--- lektion1/scripts.js
+++ lektion1/scripts.js
@@ -1,18 +1,21 @@
-const search = () => {
+function search() {
   fetch(
     "https://api.dataforsyningen.dk/adgangsadresser?vejnavn=Rødkildevej"
   ).then((response) =>
     response.json().then((data) => {
       console.log(data);
-      let html = "<ul>";
+      let html = "<div class='container'><div class='row'>";
       data.forEach((element) => {
-        html += "<li>" + element.adressebetegnelse + "</li>";
+        html +=
+          "<div class='col-6 col-sm-4 col-md-3 col-xl-2' >" +
+          element.adressebetegnelse +
+          "</div>";
       });
-      html += "</ul>";
+      html += "</div><div>";
       document.getElementById("found").innerHTML = html;
     })
   );
-};
+}
 
 window.onload = () => {
   document.getElementById("searchButton").addEventListener("click", search);
