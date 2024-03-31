cecf83fe14e4aacf8df563ab082fe53e3b79a2dd
Christian <chrbudtz@gmail.com>
2024-03-31 14:11:09 +0200

    updates for lektion 2

--- lektion2/src/routes/+page.svelte
+++ lektion2/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let input = '';
+	let result = null;
+
+	const search = async () => {
+		const response = await fetch(`https://dawa.aws.dk/adresser?q=${input}`);
+		result = await response.json();
+	};
+</script>
+
+<h1>Telemedicin</h1>
+<input bind:value={input} type="text" placeholder="Indtast adresse" />
+<button on:click={search}> Søg </button>
+<h2>Fundne Adresser</h2>
+{#if result && result.length > 0}
+	<ul>
+		{#each result as address}
+			<li>{address.adressebetegnelse}</li>
+		{/each}
+	</ul>
+{:else if result}
+	<p>Ingen adresser fundet</p>
+{/if}
